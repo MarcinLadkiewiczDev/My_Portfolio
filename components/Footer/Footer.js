7301360f13f1dc2ea99f26a6b7c698fe0fed50ef
Marcin Ladkiewicz <marcin.ladkiewicz@gmail.com>
2025-04-23 16:30:01 +0200

    Update Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -5,16 +5,16 @@ export const Footer = () => `
 <h2>Find Me</h2>
 <div>
 <a href="https://www.linkedin.com/in/marcin-ladkiewicz-026596306/" target="_blank" rel="noopener noreferrer">
-${Button("/public/icons/BW_Linkedin_glyph_svg-128.webp", "LinkedIn")}
+${Button("../public/icons/BW_Linkedin_glyph_svg-128.webp", "LinkedIn")}
 </a>
 <a href="https://www.instagram.com/martinviciouscircle/" target="_blank" rel="noopener noreferrer">
-${Button("/public/icons/Instagram_glyph_svg-128.webp", "Instagram")}
+${Button("../public/icons/Instagram_glyph_svg-128.webp", "Instagram")}
 </a>
 <a href="https://github.com/MarcinLadkiewiczDev" target="_blank" rel="noopener noreferrer">
-${Button("/public/icons/github_logo.jpeg", "GitHub")}
+${Button("../public/icons/github_logo.jpeg", "GitHub")}
 </a>
 <a href="https://discord.com/channels/@me" target="_blank" rel="noopener noreferrer">
-${Button("public/icons/FontAwesome-Brands-Discord-icon.png", "Discord")}
+${Button("../public/icons/FontAwesome-Brands-Discord-icon.png", "Discord")}
 </a>
 </div>
 `;
