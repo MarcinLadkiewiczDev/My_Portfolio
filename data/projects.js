514ea03a797de174310b29d4fd8d9782e3c02212
Marcin Ladkiewicz <marcin.ladkiewicz@gmail.com>
2025-04-23 16:35:04 +0200

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "Throne And Liberty Clone Website",
     description:
       "Recreating the website for the MMORPG game Throne and Liberty.",
-    image: "/public/ProjectFronts/ThroneAndLiberty.jpg",
+    image: "/ProjectFronts/ThroneAndLiberty.jpg",
     tech: ["HTML5", "CSS3"],
     github: "http://github.com/myuser/proyecto2",
     link: "https://throneandliberty.vercel.app/",
@@ -12,7 +12,7 @@ export const projects = [
     title: "Sapporet Front End",
     description:
       "Second project for the Masters Degree for FullStackDevelopment. FrontEnd Design for a vapeshop.",
-    image: "/public/ProjectFronts/FrontProject1.jpg",
+    image: "/ProjectFronts/FrontProject1.jpg",
     tech: ["HTML5", "CSS3"],
     github: "https://github.com/MarcinLadkiewiczDev/Proyecto_1",
     link: "https://sapporetclone.vercel.app/",
@@ -21,7 +21,7 @@ export const projects = [
     title: "One Piece Sea Emperors",
     description:
       "Little Photo Gallery of One Piece Characters Painted with DOM and adding some filter functions",
-    image: "/public/ProjectFronts/OnePieceSeaEmperors.jpg",
+    image: "/ProjectFronts/OnePieceSeaEmperors.jpg",
     tech: ["HTML5", "CSS3", "JavaScript"],
     github: "https://github.com/MarcinLadkiewiczDev/Practica_DOM_Gallery",
     link: "https://onepieceseaemperors.vercel.app/",
@@ -30,7 +30,7 @@ export const projects = [
     title: "GOT Character Selection - Async",
     description:
       "Little API proyect to practice Async with JS. Easily choose your favourite GOT character.",
-    image: "public/ProjectFronts/gotcharactersselection.jpg",
+    image: "/ProjectFronts/gotcharactersselection.jpg",
     tech: ["HTML5", "CSS3", "JavaScript"],
     github:
       "https://github.com/MarcinLadkiewiczDev/ejercicios_async_js/tree/main/ejercicio1",
@@ -40,7 +40,7 @@ export const projects = [
     title: "Random Pokemon Generator",
     description:
       "Another Async practice project with the PokeAPI. Random Pokemon each time you refresh or press the button.",
-    image: "public/ProjectFronts/randompokemongenerator.jpg",
+    image: "/ProjectFronts/randompokemongenerator.jpg",
     tech: ["HTML5", "CSS3", "JavaScript"],
     github:
       "https://github.com/MarcinLadkiewiczDev/ejercicios_async_js/tree/main/ejercicio2",
